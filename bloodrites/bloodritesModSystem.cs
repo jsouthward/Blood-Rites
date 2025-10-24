0bac252dcb94165eb6fb3b6fd9b64e2d537b1fd9
Jake <Jake@DESKTOP-7O2SQ59>
2025-10-25 00:03:38 +0100

    Add project files.

---
+++ bloodrites/bloodritesModSystem.cs
@@ -0,0 +1,29 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace bloodrites
+{
+    public class bloodritesModSystem : ModSystem
+    {
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod: " + api.Side);
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("bloodrites:hello"));
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("bloodrites:hello"));
+        }
+
+    }
+}
